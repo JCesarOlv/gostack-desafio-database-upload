3c6064dc2c2fdc9879ffd2c4942be58c1f06443c
Julio Cesar <jcesar_olv@outlook.com>
2020-12-03 15:48:04 -0300

    challenge finished

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,58 @@
-// import AppError from '../errors/AppError';
+import {getCustomRepository, getRepository } from "typeorm";
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, value, type, category }: Request ): Promise<Transaction> {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new AppError('Transaction type is invalid!');
+    }
+
+    const categoriesRepository = getRepository(Category);
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    if ('outcome'.includes(type)) {
+      const balance = await transactionsRepository.getBalance();
+      if (value > balance.total) {
+        throw new AppError('Output value greater than the total available');
+      }
+    }
+
+    let transactionCategory = await categoriesRepository.findOne({
+      where: { title: category }
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(transactionCategory);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      type,
+      value,
+      category_id: transactionCategory.id
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
+
   }
 }
 

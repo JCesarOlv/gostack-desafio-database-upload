bbd28b3c62aad902301e6933ee90f12d6b624258
Julio Cesar <jcesar_olv@outlook.com>
2020-12-15 19:32:30 -0300

    adding cors and updating models

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -27,7 +27,7 @@ class Transaction {
   @Column()
   category_id: string;
 
-  @ManyToOne(type => Category)
+  @ManyToOne(() => Category, category => category.transaction, { eager: true })
   @JoinColumn({ name: "category_id" })
   category: Category;
 
